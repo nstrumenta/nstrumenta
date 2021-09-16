6eda0bc6407ce0614ee4710f1704fd7a68548173
kayvon <kayvonghashghai@gmail.com>
2021-09-16 15:46:04 +0200

    Add eslint and prettier (we can edit prettier rules as we go)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import { Command } from 'commander'
+import { Command } from 'commander';
 
 const program = new Command()
-    .version('0.0.1')
-    .option('-d, --debug', 'output extra debugging')
-    // .command('run [algorithm] [input]', 'run algorithm on input')
+  .version('0.0.1')
+  .option('-d, --debug', 'output extra debugging');
+// .command('run [algorithm] [input]', 'run algorithm on input')
 
-program.parse(process.argv)
+program.parse(process.argv);
 
-const options = program.opts()
-if (options.debug) console.log(options)
-console.log("hello world", options)
-
+const options = program.opts();
+if (options.debug) console.log(options);
+console.log('hello world', options);
 
 // can't use redirects or pop-ups  - might use email password, or could require an nstrumenta API key
