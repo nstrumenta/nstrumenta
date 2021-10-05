7213481f67814f88dae0d3797afa5a341ed4379c
nst-t <tyler@nstrumenta.com>
2021-10-04 20:51:53 -0700

    feat(pubsub): adds publish and subscribe to channel

--- src/commands/serialport.ts
+++
@@ -1,6 +0,0 @@
-import SerialPort from 'serialport';
-
-export const SerialportList = async () => {
-  const devices = await SerialPort.list();
-  devices.forEach((device) => console.dir(device));
-};
