bc47c3fcef241955e806fdcea3d404a9651585af
kayvon <kayvonghashghai@gmail.com>
2021-12-23 20:47:56 -0800

    wip - make local=true do something for getSIGNUPLOADURL

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,3 +6,15 @@ export {
   makeBusMessageFromJsonObject,
 } from './busMessage';
 export { asyncSpawn } from './utils';
+
+// TODO: add a local bool to context to handle this; or something like that
+export const endpoints = process.env.LOCAL
+  ? {
+      GET_MACHINES: 'http://localhost:8080',
+      GET_SIGNED_UPLOAD_URL: 'http://localhost:8080',
+    }
+  : {
+      GET_MACHINES: 'https://us-central1-macro-coil-194519.cloudfunctions.net/getMachines',
+      GET_SIGNED_UPLOAD_URL:
+        'https://us-central1-macro-coil-194519.cloudfunctions.net/getSignedUploadUrl',
+    };
