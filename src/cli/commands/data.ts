31792f0133959579c120a78337a41760f6ce0fc3
Kayvon Ghashghai <kayvon@users.noreply.github.com>
2022-06-22 05:40:12 -0700

    feat(cli): upload file to project data (#116)
    * feat(cli): upload file to project data
    
    * feat(cli): allows uploading of multiple files at a time to data; fixes 1:1 relationship of storage to db; requires dataId attribute to be returned from getUploadDataUrl function

--- src/cli/commands/data.ts
+++ src/cli/commands/data.ts
@@ -1,6 +1,8 @@
 import axios, { Axios, AxiosRequestConfig } from 'axios';
 import { resolveApiKey } from '../utils';
 import { endpoints, ObjectTypes } from '../../shared';
+import { readFile, stat } from 'fs/promises';
+import ErrnoException = NodeJS.ErrnoException;
 
 export interface ModuleMeta {
   filePath: string;
@@ -28,4 +30,81 @@ export const List = async (_: unknown, options: DataListOptions) => {
   } catch (error) {
     console.log(`Problem fetching data ${(error as Error).name}`);
   }
+};
+
+export const Upload = async (filenames: string[]) => {
+  if (filenames.length === 0) {
+    return console.log('Please specify at least one filename');
+  }
+
+  try {
+    for (const file of filenames) {
+      await stat(file);
+    }
+  } catch (error) {
+    if ((error as ErrnoException).code === 'ENOENT') {
+      console.warn(`Request canceled: ${(error as ErrnoException).path} does not exist`);
+      return;
+    }
+    throw error;
+  }
+
+  let dataId: string | undefined;
+  let results = [];
+  for (const filename of filenames) {
+    console.log('upload', { filename, dataId });
+    const response = await uploadFile({ filename, dataId });
+    results.push(response);
+    if (!dataId) {
+      dataId = response.dataId;
+    }
+  }
+};
+
+interface UploadResponse {
+  filename: string;
+  remoteFilePath: string;
+  dataId: string;
+}
+
+export const uploadFile = async ({
+  filename,
+  dataId,
+}: {
+  filename: string;
+  dataId?: string;
+}): Promise<UploadResponse> => {
+  const apiKey = resolveApiKey();
+  let fileBuffer;
+  let size;
+  let url;
+  let remoteFilePath;
+
+  fileBuffer = await readFile(filename);
+  size = fileBuffer.length;
+
+  const config: AxiosRequestConfig = {
+    method: 'post',
+    headers: { 'x-api-key': apiKey },
+    data: { name: filename, size, dataId },
+  };
+  let response = await axios(endpoints.GET_UPLOAD_DATA_URL, config);
+
+  url = response.data?.uploadUrl;
+  remoteFilePath = response.data?.remoteFilePath;
+  dataId = response.data?.dataId;
+  if (typeof dataId === 'undefined') {
+    throw new Error('dataId is invalid');
+  }
+
+  await axios.put(url, fileBuffer, {
+    maxBodyLength: Infinity,
+    maxContentLength: Infinity,
+    headers: {
+      contentLength: `${size}`,
+      contentLengthRange: `bytes 0-${size - 1}/${size}`,
+    },
+  });
+
+  return { filename, remoteFilePath, dataId };
 };
