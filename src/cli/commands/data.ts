d5e33ef590aa85a0ccc280b47de5475dc13d5b03
Kayvon Ghashghai <kayvon@users.noreply.github.com>
2022-06-29 06:49:53 -0700

    feat(cli): adds data > query (#118)
    * feat(cli): wip: adds data get (query only so far, by tag or filename)
    
    * feat(cli): updates data upload to set metadata on dataId bucket to facilitate filtering
    
    * feat(cli): wip: adds data query command (so far can query by tag -> finds data tagged with at least one of supplied tags) (depends on queryData function)
    
    * feat(cli): adds data:get command

--- src/cli/commands/data.ts
+++ src/cli/commands/data.ts
@@ -1,8 +1,13 @@
-import axios, { Axios, AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { resolveApiKey } from '../utils';
 import { endpoints, ObjectTypes } from '../../shared';
-import { readFile, stat } from 'fs/promises';
+import { readFile, stat, writeFile, mkdir, access } from 'fs/promises';
 import ErrnoException = NodeJS.ErrnoException;
+import { createWriteStream } from 'fs';
+import { pipeline as streamPipeline } from 'stream';
+import { promisify } from 'util';
+
+const pipeline = promisify(streamPipeline);
 
 export interface ModuleMeta {
   filePath: string;
@@ -70,7 +75,7 @@ export const Upload = async (filenames: string[], { tags }: { tags?: string[] })
   try {
     const apiKey = resolveApiKey();
 
-    const metadata = { tags, filenames };
+    const metadata = { tags, files: results };
     await axios(endpoints.SET_DATA_METADATA, {
       method: 'post',
       data: { metadata, merge: true, dataId },
@@ -135,4 +140,113 @@ export const uploadFile = async ({
   });
 
   return { filename, remoteFilePath, dataId };
+};
+
+export interface DataQueryOptions {
+  tag?: string[];
+  before?: string;
+  after?: string;
+  limit?: string;
+  file?: string[];
+}
+
+export interface DataFile {
+  filename: string;
+  dataId: string;
+  remoteFilePath: string;
+}
+
+export type DataQueryResponse = {
+  id: string;
+  data: {
+    filePath: string;
+    tags: string[];
+    files: DataFile[];
+  };
+}[];
+
+export const query = async ({
+  file: filenames,
+  tag: tags,
+  before: b,
+  after: a,
+  limit: l = '1',
+}: DataQueryOptions): Promise<DataQueryResponse> => {
+  const apiKey = resolveApiKey();
+  const before = b ? parseInt(b, 10) : undefined;
+  const after = a ? parseInt(a, 10) : undefined;
+  const limit = l ? parseInt(l, 10) : undefined;
+
+  const data = { tags, before, after, limit, filenames };
+
+  const config: AxiosRequestConfig = {
+    method: 'post',
+    headers: { 'x-api-key': apiKey },
+    data,
+  };
+
+  try {
+    const response = await axios(endpoints.QUERY_DATA, config);
+
+    return response.data;
+  } catch (error) {
+    console.log(`Something went wrong: ${(error as Error).message}`);
+
+    return [];
+  }
+};
+
+export const Query = async (options: DataQueryOptions) => {
+  const data = await query(options);
+  console.log(JSON.stringify(data, undefined, 2));
+};
+
+export const Get = async (options: DataQueryOptions) => {
+  const data = await query(options);
+  const downloads = data.map(async (value) => {
+    const {
+      id,
+      data: { files },
+    } = value;
+
+    // Create directory for this dataId
+    const dataIdFolder = `./${id}`;
+    try {
+      await access(dataIdFolder);
+    } catch {
+      await mkdir(dataIdFolder, { recursive: true });
+    }
+
+    const downloadPromises = files.map(async ({ remoteFilePath }) => {
+      const remotePathInProject = remoteFilePath.replace(/^projects\/[^/]+\//, '');
+      const filename = `${dataIdFolder}/${remoteFilePath.split('/').pop()}`;
+      try {
+        await stat(filename!);
+        console.log(`using existing version of ${filename}`);
+        return filename;
+      } catch {
+        const downloadUrlData = { path: remotePathInProject };
+        const downloadUrlConfig = {
+          headers: { 'x-api-key': resolveApiKey(), 'content-type': 'application/json' },
+        };
+        const getDownloadUrl = await axios.post(
+          endpoints.GET_PROJECT_DOWNLOAD_URL,
+          downloadUrlData,
+          downloadUrlConfig
+        );
+        const downloadUrl = getDownloadUrl.data;
+
+        const download = await axios.get(downloadUrl, { responseType: 'stream' });
+
+        let writeStream;
+        writeStream = createWriteStream(filename);
+        await pipeline(download.data, writeStream);
+        return filename;
+      }
+    });
+    return Promise.allSettled(downloadPromises);
+  });
+
+  const results = await Promise.allSettled(downloads);
+  console.log(JSON.stringify(results, undefined, 2));
 };
