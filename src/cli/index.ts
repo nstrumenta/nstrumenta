aea176dafe4b7bfa97c13c27f28b97af4050c976
Kayvon Ghashghai <kayvon@users.noreply.github.com>
2022-06-29 06:49:53 -0700

    feat(cli): adds data > query (#118)
    * feat(cli): wip: adds data get (query only so far, by tag or filename)
    
    * feat(cli): updates data upload to set metadata on dataId bucket to facilitate filtering
    
    * feat(cli): wip: adds data query command (so far can query by tag -> finds data tagged with at least one of supplied tags) (depends on queryData function)
    
    * feat(cli): adds data:get command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -22,7 +22,12 @@ import {
 import { ListMachines } from './commands/machines';
 import { List, Publish, Run } from './commands/module';
 import { Send, Subscribe } from './commands/pubsub';
-import { List as ListData, Upload as UploadData } from './commands/data';
+import {
+  List as ListData,
+  Upload as UploadData,
+  Query as QueryData,
+  Get as GetData,
+} from './commands/data';
 import axios from 'axios';
 import { resolveApiKey } from './utils';
 
@@ -161,6 +166,26 @@ dataCommand
   .argument('<filename...>', 'filename to upload')
   .description('Upload file to project data')
   .action(UploadData);
+dataCommand
+  .command('query')
+  .option('-t, --tag <tag...>')
+  .option('-f, --file <file...>')
+  .option('-b, --before <before>', 'before timestamp')
+  .option('-a, --after <after>', 'after timestamp')
+  .option('-l, --limit <limit>', 'default to 1')
+  // .argument('<filename...>', 'filenames to filter by')
+  .description('Get data by name, tags, or date range')
+  .action(QueryData);
+dataCommand
+  .command('get')
+  .option('-t, --tag <tag...>')
+  .option('-f, --file <file...>')
+  .option('-b, --before <before>', 'before timestamp')
+  .option('-a, --after <after>', 'after timestamp')
+  .option('-l, --limit <limit>', 'default to 1')
+  // .argument('<filename...>', 'filenames to filter by')
+  .description('Get data by name, tags, or date range')
+  .action(GetData);
 
 const adminUtilsCommand = program.command('admin-utils', '', { hidden: true });
 adminUtilsCommand
