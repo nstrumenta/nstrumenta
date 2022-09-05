95f5eafbaf89eec7fd328d0071b9267ae8915d66
Kayvon Ghashghai <kayvon@users.noreply.github.com>
2022-09-05 11:47:10 -0700

    feat(client:node): connect returns a promise once client is verified, not just 'open' event on the websocket (#140)

--- src/nodejs/client.ts
+++ src/nodejs/client.ts
@@ -51,91 +51,94 @@ export class NstrumentaClient implements NstrumentaClientBase {
     return;
   }
 
-  public async connect(connectOptions: ConnectOptions) {
-    const { wsUrl, apiKey, verify = true } = connectOptions;
-    if (this.reconnection.attempts > 100) {
-      throw new Error('Too many reconnection attempts, stopping');
-    }
-    this.apiKey = apiKey || resolveApiKey();
-    if (!this.apiKey) {
-      throw new Error(
-        'nstrumenta api key is missing, pass it as an argument to NstrumentaClient.connect({apiKey: "your key"}) for javascript clients in the browser, or set the NSTRUMENTA_API_KEY environment variable get a key from your nstrumenta project settings https://nstrumenta.com/projects/ *your projectId here* /settings'
-      );
-    }
-    let token = 'unverified';
-    if (verify) {
-      try {
-        token = await getToken(this.apiKey);
-        // initiate the storage service for file upload/download
-        this.storage = new StorageService({ apiKey: this.apiKey });
-      } catch (error) {
-        console.error((error as Error).message);
-        throw error;
+  public async connect(connectOptions: ConnectOptions): Promise<Connection> {
+    return new Promise(async (resolve, reject) => {
+      console.log('connecting to nstrumenta');
+      const { wsUrl, apiKey, verify = true } = connectOptions;
+      if (this.reconnection.attempts > 100) {
+        throw new Error('Too many reconnection attempts, stopping');
       }
-    }
-
-    this.ws = new WebSocket(wsUrl);
-    this.ws.binaryType = 'arraybuffer';
-    this.ws.addEventListener('open', async () => {
-      console.log(`client websocket opened <${wsUrl}>`);
-      this.ws?.send(token);
-      this.reconnection.attempts = 0;
-      this.connection.status = ClientStatus.CONNECTING;
-    });
-    this.ws.addEventListener('close', (status) => {
-      this.connection.status = ClientStatus.DISCONNECTED;
-      this.listeners.get('close')?.forEach((callback) => callback());
-      console.log(
-        `client websocket closed ${wsUrl} code:${status.code} wasClean:${status.wasClean}`
-      );
-      this.subscriptions.clear();
-      // reconnect on close
-      if (this.reconnection.hasVerified) {
-        setTimeout(() => {
-          console.log(`attempting to reconnect, attempts: ${this.reconnection.attempts}`);
-          this.connect(connectOptions);
-        }, this.rollOff(this.reconnection.attempts));
-        this.reconnection.attempts += 1;
+      this.apiKey = apiKey || resolveApiKey();
+      if (!this.apiKey) {
+        throw new Error(
+          'nstrumenta api key is missing, pass it as an argument to NstrumentaClient.connect({apiKey: "your key"}) for javascript clients in the browser, or set the NSTRUMENTA_API_KEY environment variable get a key from your nstrumenta project settings https://nstrumenta.com/projects/ *your projectId here* /settings'
+        );
       }
-    });
-    this.ws.addEventListener('error', (err) => {
-      console.log(`Error in websocket connection`);
-      this.connection.status = ClientStatus.ERROR;
-    });
-    this.ws.addEventListener('message', (event) => {
-      const wireMessage = event.data as ArrayBuffer;
-      //console.log('nstClient received message', wireMessage);
-      let deserializedMessage;
-      try {
-        deserializedMessage = deserializeWireMessage(wireMessage as ArrayBuffer);
-      } catch (error) {
-        console.log(`Couldn't deserialize message ${JSON.stringify(error)}`);
-        return;
-      }
-      const { channel, busMessageType, contents } = deserializedMessage;
-      if (channel == '_nstrumenta') {
-        const { verified, error, clientId } = contents;
-        if (error) {
-          console.error(error);
-        }
-        if (verified) {
-          this.connection.status = ClientStatus.CONNECTED;
-          this.reconnection.hasVerified = true;
-          this.listeners.get('open')?.forEach((callback) => callback());
-          this.messageBuffer.forEach((message) => {
-            this.ws?.send(message);
-          });
-          this.messageBuffer = [];
-          this.clientId = clientId;
+      let token = 'unverified';
+      if (verify) {
+        try {
+          token = await getToken(this.apiKey);
+          // initiate the storage service for file upload/download
+          this.storage = new StorageService({ apiKey: this.apiKey });
+        } catch (error) {
+          console.error((error as Error).message);
+          throw error;
         }
       }
 
-      this.subscriptions.get(channel)?.forEach((subscription) => {
-        subscription(contents);
+      this.ws = new WebSocket(wsUrl);
+      this.ws.binaryType = 'arraybuffer';
+      this.ws.addEventListener('open', async () => {
+        console.log(`client websocket opened <${wsUrl}>`);
+        this.ws?.send(token);
+        this.reconnection.attempts = 0;
+        this.connection.status = ClientStatus.CONNECTING;
+      });
+      this.ws.addEventListener('close', (status) => {
+        this.connection.status = ClientStatus.DISCONNECTED;
+        this.listeners.get('close')?.forEach((callback) => callback());
+        console.log(
+          `client websocket closed ${wsUrl} code:${status.code} wasClean:${status.wasClean}`
+        );
+        this.subscriptions.clear();
+        // reconnect on close
+        if (this.reconnection.hasVerified) {
+          setTimeout(() => {
+            console.log(`attempting to reconnect, attempts: ${this.reconnection.attempts}`);
+            this.connect(connectOptions);
+          }, this.rollOff(this.reconnection.attempts));
+          this.reconnection.attempts += 1;
+        }
+      });
+      this.ws.addEventListener('error', (err) => {
+        console.log(`Error in websocket connection`);
+        this.connection.status = ClientStatus.ERROR;
+        reject(this.connection);
+      });
+      this.ws.addEventListener('message', (event) => {
+        const wireMessage = event.data as ArrayBuffer;
+        //console.log('nstClient received message', wireMessage);
+        let deserializedMessage;
+        try {
+          deserializedMessage = deserializeWireMessage(wireMessage as ArrayBuffer);
+        } catch (error) {
+          console.log(`Couldn't deserialize message ${JSON.stringify(error)}`);
+          return;
+        }
+        const { channel, busMessageType, contents } = deserializedMessage;
+        if (channel == '_nstrumenta') {
+          const { verified, error, clientId } = contents;
+          if (error) {
+            console.error(error);
+          }
+          if (verified) {
+            this.connection.status = ClientStatus.CONNECTED;
+            this.reconnection.hasVerified = true;
+            this.listeners.get('open')?.forEach((callback) => callback());
+            this.messageBuffer.forEach((message) => {
+              this.ws?.send(message);
+            });
+            this.messageBuffer = [];
+            this.clientId = clientId;
+            resolve(this.connection);
+          }
+        }
+
+        this.subscriptions.get(channel)?.forEach((subscription) => {
+          subscription(contents);
+        });
       });
     });
-
-    return this.connection;
   }
 
   private rollOff(attempts: number) {
