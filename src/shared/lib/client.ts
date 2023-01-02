17ea3d59e71675e22b9f0e40914d38f02c5cfa85
nst-t <41758588+nst-t@users.noreply.github.com>
2023-01-02 13:27:32 -0800

    feat(clients): implements shared code in shared/lib (#160)
    * feat(clients): implements shared code in shared/lib
    
    * adds remove listeners to shutdown

--- src/shared/lib/client.ts
+++ src/shared/lib/client.ts
@@ -1,11 +1,29 @@
-import { WebSocket } from 'ws';
-import { DataQueryOptions, DataQueryResponse } from '../index';
+import { Mcap0Types } from '@mcap/core';
+import axios, { AxiosRequestConfig } from 'axios';
+import type WebSocket from 'ws';
+import {
+  DataQueryOptions,
+  DataQueryResponse,
+  getEndpoints,
+  makeBusMessageFromJsonObject,
+} from '../index';
+import { makeBusMessageFromBuffer } from './busMessage';
+
+export interface LogConfig {
+  header: Mcap0Types.Header;
+  channels: Array<{
+    schema: { title: string; type: 'object'; properties: Record<string, unknown> };
+    channel: Omit<Mcap0Types.Channel, 'id' | 'schemaId' | 'metadata'>;
+  }>;
+}
 
 export type ListenerCallback = (event?: any) => void;
 export type SubscriptionCallback = (message?: any) => void;
 
+export type WebSocketLike = WebSocket;
+
 export interface ConnectOptions {
-  nodeWebSocket?: new (url: string) => WebSocket;
+  nodeWebSocket?: new (url: string) => WebSocketLike;
   wsUrl: string;
   apiKey?: string;
   verify?: boolean;
@@ -25,26 +43,164 @@ export interface Connection {
   status: ClientStatus;
 }
 
-export interface NstrumentaClientBase {
-  connection: Connection;
+export type Reconnection = {
+  hasVerified: boolean;
+  attempts: number;
+  timeout: ReturnType<typeof setTimeout> | null;
+};
 
-  shutdown(): Promise<void>;
+export const getToken = async (apiKey: string): Promise<string> => {
+  const headers = {
+    'x-api-key': apiKey,
+    'Content-Type': 'application/json',
+  };
+  try {
+    // https://stackoverflow.com/questions/69169492/async-external-function-leaves-open-handles-jest-supertest-express
+    const { data } = await axios.get<{ token: string }>(getEndpoints('prod').GET_TOKEN, {
+      headers,
+    });
+    return data.token;
+  } catch (err) {
+    const message = `Problem getting token, check api key, err: ${(err as Error).message}`;
+    throw new Error(message);
+  }
+};
 
-  connect(connectOptions: ConnectOptions): Promise<Connection>;
+export abstract class NstrumentaClientBase {
+  public ws: WebSocketLike | null = null;
+  public apiKey: string | null = null;
+  public listeners: Map<string, Array<ListenerCallback>>;
+  public subscriptions: Map<string, SubscriptionCallback[]>;
+  public reconnection: Reconnection = { hasVerified: false, attempts: 0, timeout: null };
+  public messageBuffer: Array<ArrayBufferLike>;
+  private datalogs: Map<string, Array<string>>;
+  public clientId: string | null = null;
+  private endpoints;
 
-  send(channel: string, message: Record<string, unknown>): void;
+  public connection: Connection = { status: ClientStatus.INIT };
 
-  sendBuffer(channel: string, buffer: ArrayBufferLike): void;
+  constructor() {
+    this.listeners = new Map();
+    this.subscriptions = new Map();
+    this.datalogs = new Map();
+    this.messageBuffer = [];
+    this.endpoints = getEndpoints('prod');
+    this.addSubscription = this.addSubscription.bind(this);
+    this.addListener = this.addListener.bind(this);
+    this.connect = this.connect.bind(this);
+  }
 
-  addSubscription(channel: string, callback: SubscriptionCallback): void;
+  async shutdown() {
+    this.listeners.clear();
+    this.subscriptions.clear();
+    this.datalogs.clear();
+    this.messageBuffer = [];
+    if (this.reconnection.timeout) {
+      clearTimeout(this.reconnection.timeout);
+      this.reconnection.timeout = null;
+    }
+    this.ws?.removeAllListeners();
+    this.ws?.close();
+    return;
+  }
 
-  addListener(eventType: 'open' | 'close', callback: ListenerCallback): void;
+  public abstract connect(connectOptions?: ConnectOptions): Promise<Connection>;
 
-  startLog(name: string, channels: string[]): Promise<void>;
+  public rollOff(attempts: number) {
+    if (attempts == 0) return 0;
+    // rolls off exponentially until a max of 30 minutes
+    // 100 attempts is about 1.5 days
+    return Math.min(Math.pow(attempts, 2) * 1000, 30 * 60 * 1000);
+  }
 
-  finishLog(name: string): Promise<void>;
+  public send(channel: string, message: Record<string, unknown>) {
+    this.bufferedSend(makeBusMessageFromJsonObject(channel, message));
+  }
 
-  storage?: BaseStorageService;
+  public sendBuffer(channel: string, buffer: ArrayBufferLike) {
+    this.bufferedSend(makeBusMessageFromBuffer(channel, buffer));
+  }
+
+  private bufferedSend(message: ArrayBufferLike) {
+    // buffers messages sent before initial connection
+    if (!(this.ws?.readyState === this.ws?.OPEN)) {
+      console.log('adding to messageBuffer, length:', this.messageBuffer.length);
+      this.messageBuffer.push(message);
+    } else {
+      this.ws?.send(message);
+    }
+  }
+
+  public addSubscription(channel: string, callback: SubscriptionCallback) {
+    console.log(`Nstrumenta client subscribe <${channel}>`);
+    const channelSubscriptions = this.subscriptions.get(channel) || [];
+    channelSubscriptions.push(callback);
+    this.subscriptions.set(channel, channelSubscriptions);
+
+    this.bufferedSend(makeBusMessageFromJsonObject('_command', { command: 'subscribe', channel }));
+  }
+
+  public addListener(eventType: 'open' | 'close', callback: ListenerCallback) {
+    if (!this.listeners.get(eventType)) {
+      this.listeners.set(eventType, []);
+    }
+    const listenerCallbacks = this.listeners.get(eventType);
+    if (listenerCallbacks) {
+      listenerCallbacks.push(callback);
+    }
+  }
+
+  public addStringToDataLog(logName: string, entry: string) {
+    const log = this.datalogs.get(logName);
+    if (!log) {
+      console.log(`starting new log: ${logName}`);
+      this.datalogs.set(logName, [entry]);
+    } else {
+      log.push(entry);
+    }
+  }
+
+  public async uploadData(path: string, data: Blob, meta: Record<string, string>) {
+    const size = data.size;
+    let url;
+    const response = await axios.post(
+      this.endpoints.GET_UPLOAD_URL,
+      {
+        path,
+        size,
+        meta,
+      },
+      {
+        headers: {
+          contentType: 'application/json',
+          'x-api-key': this.apiKey!,
+        },
+      }
+    );
+
+    url = response.data?.uploadUrl;
+
+    await axios.put(url, data, {
+      maxBodyLength: Infinity,
+      maxContentLength: Infinity,
+      headers: {
+        contentType: 'application/octet-stream',
+        contentLength: `${size}`,
+        contentLengthRange: `bytes 0-${size - 1}/${size}`,
+      },
+    });
+  }
+
+  public async startLog(name: string, channels: string[], config?: LogConfig) {
+    this.send('_nstrumenta', { command: 'startLog', name, channels, config });
+  }
+
+  public async finishLog(name: string) {
+    console.log('finish log');
+    this.send('_nstrumenta', { command: 'finishLog', name });
+  }
+
+  storage?: StorageService;
 }
 
 export interface StorageUploadParameters {
@@ -54,14 +210,124 @@ export interface StorageUploadParameters {
   dataId?: string;
 }
 
-export interface BaseStorageService {
-  list(type: string): Promise<string[]>;
+export class StorageService {
+  private apiKey: string;
+  private endpoints;
 
-  upload({ filename, data, meta, dataId }: StorageUploadParameters): Promise<void>;
+  constructor(props: { apiKey: string }) {
+    this.apiKey = props.apiKey;
+    this.endpoints = getEndpoints('prod');
+  }
 
-  download<T>(type: string, path: string): Promise<T>;
+  async download(path: string): Promise<Blob> {
+    const response = await axios(this.endpoints.GET_PROJECT_DOWNLOAD_URL, {
+      method: 'post',
+      headers: { 'x-api-key': this.apiKey, 'content-type': 'application/json' },
+      data: { path },
+    });
+    console.log('REQ:', response.request);
 
-  download(type: string, path: string): Promise<unknown>;
+    const { data: fetched } = await axios(response.data, {
+      method: 'GET',
+      responseType: 'blob',
+    });
 
-  query(options: DataQueryOptions): Promise<DataQueryResponse>;
+    return fetched;
+  }
+
+  async query({
+    filenames,
+    tag: tags,
+    before,
+    after,
+    limit = 1,
+    metadata: metadataOriginal,
+  }: DataQueryOptions): Promise<DataQueryResponse> {
+    const metadata =
+      typeof metadataOriginal === 'string'
+        ? JSON.parse(metadataOriginal)
+        : typeof metadataOriginal === 'object'
+        ? metadataOriginal
+        : {};
+
+    const data = { tags, before, after, limit, filenames, metadata };
+
+    const config: AxiosRequestConfig = {
+      method: 'post',
+      headers: { 'x-api-key': this.apiKey },
+      data,
+    };
+
+    try {
+      const response = await axios(this.endpoints.QUERY_DATA, config);
+
+      return response.data;
+    } catch (error) {
+      console.log(`Something went wrong: ${(error as Error).message}`);
+
+      return [];
+    }
+  }
+
+  async list(type: string): Promise<string[]> {
+    let response = await axios(this.endpoints.LIST_STORAGE_OBJECTS, {
+      method: 'post',
+      headers: { 'x-api-key': this.apiKey, 'content-type': 'application/json' },
+      data: { type },
+    });
+
+    return response.data;
+  }
+
+  public async upload({ filename, data, meta, dataId: explicitDataId }: StorageUploadParameters) {
+    const size = data.size;
+    let url;
+    let dataId = explicitDataId;
+
+    if (!dataId) {
+      const res = await axios(this.endpoints.GENERATE_DATA_ID, {
+        headers: { 'x-api-key': this.apiKey!, method: 'post' },
+      });
+      dataId = res.data.dataId;
+    }
+    const config: AxiosRequestConfig = {
+      method: 'post',
+      headers: {
+        'x-api-key': this.apiKey!,
+        'Content-Type': 'application/json',
+      },
+      data: {
+        name: filename,
+        dataId,
+        size,
+        metadata: meta,
+      },
+    };
+
+    let response = await axios(this.endpoints.GET_UPLOAD_DATA_URL, config);
+
+    url = response.data?.uploadUrl;
+    if (!url) {
+      console.warn(`no upload url returned, can't upload ${filename}`);
+      console.log(response.data);
+      return;
+    }
+
+    const remoteFilePath = response.data?.remoteFilePath;
+    // const buffer = await data.arrayBuffer();
+    const uploadConfig: AxiosRequestConfig = {
+      maxBodyLength: Infinity,
+      maxContentLength: Infinity,
+      headers: {
+        contentLength: `${size}`,
+        contentLengthRange: `bytes 0-${size - 1}/${size}`,
+        'content-type': 'application/octet-stream',
+      },
+      url,
+      method: 'PUT',
+      data,
+    };
+    console.log({ remoteFilePath, uploadConfig });
+    await axios(uploadConfig);
+  }
 }
