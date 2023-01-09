78ef39656a2d8b1c382faa02784563a2d02acbe7
nst-t <tyler@nstrumenta.com>
2023-01-08 18:24:59 -0800

    removes worker

--- src/nodejs/video/examples/server-demo/src/context/room.ts
+++ src/nodejs/video/examples/server-demo/src/context/room.ts
@@ -1,10 +1,11 @@
 import { v4 } from 'uuid';
 import { Room } from '../../../../../../shared/video/packages/core/src';
 import { RTCIceCandidate, RTCSessionDescription } from 'werift';
+// import { workerThreadsWrapper, wrap } from 'airpc';
+// import { Worker } from 'worker_threads';
 
 // const workerPath = process.argv[3];
-// const workerLoaderPath =
-//   process.argv[2] === "prod" ? workerPath : process.argv[2];
+// const workerLoaderPath = process.argv[2] === 'prod' ? workerPath : process.argv[2];
 
 export class RoomManager {
   rooms: { [name: string]: Room } = {};
