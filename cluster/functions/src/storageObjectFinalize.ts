aa495b120b9eb70b6f3415b6bd6b77adfcfaaaf7
nst-t <tyler@nstrumenta.com>
2023-07-16 22:32:07 -0700

    fixes broken refactor

--- cluster/functions/src/storageObjectFinalize.ts
+++ cluster/functions/src/storageObjectFinalize.ts
@@ -3,20 +3,24 @@ import { Firestore } from '@google-cloud/firestore';
 import { File } from '@google-cloud/storage';
 import path from 'path';
 
-const serviceAccount = JSON.parse(atob(process.env.SERVICE_ACCOUNT_CREDENTIALS!))
+const serviceAccount = JSON.parse(atob(process.env.SERVICE_ACCOUNT_CREDENTIALS!));
 
-;
+const firestore = new Firestore({
+  projectId: serviceAccount.project_id,
+  credentials: {
+    client_email: serviceAccount.client_email,
+    private_key: serviceAccount.private_key,
+  },
+});
 
 export const storageObjectFinalize = async (file: File, context: CloudFunctionsContext) => {
   if (context.eventType === 'google.storage.object.finalize') {
-    await firestore
-      .doc(file.name)
-      .set({
-        name: path.basename(file.name),
-        lastModified: Date.now(),
-        filePath: file.name,
-        size: (file as any).size,
-        file,
-      });
+    await firestore.doc(file.name).set({
+      name: path.basename(file.name),
+      lastModified: Date.now(),
+      filePath: file.name,
+      size: (file as any).size,
+      file,
+    });
   }
 };
