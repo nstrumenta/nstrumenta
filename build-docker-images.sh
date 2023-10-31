1745d0b81ea049e804470b7fa7bb36ace244a185
Tyler Bryant <tyler@nstrumenta.com>
2023-10-31 09:33:24 +0000

    adds variable argument for --push

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -2,16 +2,22 @@
 PACKAGE_VERSION=$(cat package.json | jq -r '.version')
 echo "package version $PACKAGE_VERSION"
 
+if [ -n "$BUILDX_ARGS" ]; then
+    echo "building with BUILDX_ARGS $BUILDX_ARGS"
+fi
+
+exit
+
 # base
 docker buildx build \
-    --push \
+    $BUILDX_ARGS \
     --platform linux/arm64,linux/amd64 \
     --tag nstrumenta/base:$PACKAGE_VERSION \
     .
 
 # agent
 docker buildx build \
-    --push \
+    $BUILDX_ARGS \
     --platform linux/arm64,linux/amd64 \
     --tag nstrumenta/agent:$PACKAGE_VERSION \
     -f ./cluster/agent/Dockerfile \
@@ -20,7 +26,7 @@ docker buildx build \
 # server
 pushd cluster/server
 docker buildx build \
-    --push \
+    $BUILDX_ARGS \
     --platform linux/arm64,linux/amd64 \
     --build-arg BASE_TAG=$PACKAGE_VERSION \
     --tag nstrumenta/server:$PACKAGE_VERSION \
@@ -28,23 +34,19 @@ docker buildx build \
 popd
 
 # data-job-runner
-pushd cluster/data-job-runner
 docker buildx build \
-    --push \
+    $BUILDX_ARGS \
     --platform linux/arm64,linux/amd64 \
     --build-arg BASE_TAG=$PACKAGE_VERSION \
     --tag nstrumenta/data-job-runner:$PACKAGE_VERSION \
+    -f ./cluster/data-job-runner/Dockerfile \
     .
-popd
 
 # developer
-pushd cluster/developer
 docker buildx build \
-    --push \
+    $BUILDX_ARGS \
     --platform linux/arm64,linux/amd64 \
     --build-arg BASE_TAG=$PACKAGE_VERSION \
     --tag nstrumenta/developer:$PACKAGE_VERSION \
+    -f ./cluster/developer/Dockerfile \
     .
-popd
-
-
