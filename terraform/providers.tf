4fb5e05282746f1f1ac988255eb27cc9021c164c
Tyler Bryant <tyler@nstrumenta.com>
2023-11-19 00:08:20 +0000

    fix(functions): fixes delete,  fixes compute agent for secret

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,6 +16,6 @@ provider "google-beta" {
 # Configures the provider to not use the resource block's specified project for quota checks.
 # This provider should only be used during project creation and initializing services.
 provider "google-beta" {
-  alias = "no_user_project_override"
+  alias                 = "no_user_project_override"
   user_project_override = false
 }
