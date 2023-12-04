25c738bdceece6131e971aa75a040a70374f3e20
nst-t <tyler@nstrumenta.com>
2023-12-04 01:25:46 +0000

    quicker timeout

--- cluster/integration-tests/cli/client/app/tests/cloud-run.test.ts
+++ cluster/integration-tests/cli/client/app/tests/cloud-run.test.ts
@@ -125,7 +125,7 @@ esac
     const result = await pollNstrumenta({
       matchString: uploadFileName,
       interval: 5_000,
-      timeout: 180_000,
+      timeout: 30_000,
     });
     await expect(result).toBeTruthy();
 
@@ -138,9 +138,9 @@ esac
     const deleteResult = await pollNstrumenta({
       matchString: uploadFileName,
       interval: 5_000,
-      timeout: 180_000,
+      timeout: 30_000,
       inverseMatch: true,
     });
     await expect(deleteResult).toBeTruthy();
-  }, 360_000);
+  }, 60_000);
 });
