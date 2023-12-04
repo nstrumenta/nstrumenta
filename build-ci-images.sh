7050d5a5223c45a7cd5804c7e3d1e0876878570f
nst-t <tyler@nstrumenta.com>
2023-12-04 01:30:54 +0000

    adds building of ci images for integration tests

---
+++ build-ci-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+# agent
+docker buildx build \
+docker build -t $REPOSITORY/agent:$VERSION_TAG .
+docker push $REPOSITORY/agent:$VERSION_TAG
+
+# data-job-runner
+docker build -t $REPOSITORY/data-job-runner:$VERSION_TAG -f ./cluster/data-job-runner/Dockerfile .
+docker push $REPOSITORY/data-job-runner:$VERSION_TAG
+
+# server
+pushd cluster/server
+docker build -t $REPOSITORY/server:$VERSION_TAG .
+docker push $REPOSITORY/server:$VERSION_TAG
+popd
