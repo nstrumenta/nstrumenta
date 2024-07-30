eea2a5c7d6726fff9d59be63c5f9e93d73550f69
Tyler Bryant <tyler@nstrumenta.com>
2024-07-30 16:47:38 +0000

    feat(data): adds support for data dirname

--- cluster/functions/src/storageObjectFinalize.ts
+++ cluster/functions/src/storageObjectFinalize.ts
@@ -1,10 +1,11 @@
 import { Firestore } from '@google-cloud/firestore';
 import { CloudFunctionsContext } from '@google-cloud/functions-framework';
 import path from 'path';
+import crypto from 'crypto';
 
-const serviceKeyJson = process.env.GCLOUD_SERVICE_KEY
-if (serviceKeyJson == undefined) throw new Error('GCLOUD_SERVICE_KEY undefined')
-const serviceAccount = JSON.parse(serviceKeyJson)
+const serviceKeyJson = process.env.GCLOUD_SERVICE_KEY;
+if (serviceKeyJson == undefined) throw new Error('GCLOUD_SERVICE_KEY undefined');
+const serviceAccount = JSON.parse(serviceKeyJson);
 
 const firestore = new Firestore({
   projectId: serviceAccount.project_id,
@@ -14,13 +15,32 @@ const firestore = new Firestore({
   },
 });
 
+function generateHash(input: string): { dirname: string; documentPath: string } {
+  const hash = crypto.createHash('sha256').update(input).digest('hex');
+
+  // dirname is the directory of the file relative to the project data folder
+  const segments = input.split(path.sep);
+  const basePath = path.join(segments[0], segments[1], segments[2]);
+  const dirname = path.relative(basePath, path.dirname(input));
+
+  const documentPath = path.join(basePath, hash);
+
+  return { dirname, documentPath };
+}
+
 export const storageObjectFinalize = async (
   file: { name: string },
   context: CloudFunctionsContext
 ) => {
   if (context.eventType === 'google.storage.object.finalize') {
-    await firestore.doc(file.name).set({
+    if (file.name.endsWith('/')) {
+      console.log('The trigger file is a directory. Skipping processing.');
+      return;
+    }
+    const { documentPath, dirname } = generateHash(file.name);
+    await firestore.doc(documentPath).set({
       name: path.basename(file.name),
+      dirname,
       lastModified: Date.now(),
       filePath: file.name,
       size: (file as any).size,
