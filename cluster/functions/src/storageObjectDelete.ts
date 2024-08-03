f32306c5a0a13c2cd1e3dfeec63ed7753c6020f6
nst-t <41758588+nst-t@users.noreply.github.com>
2024-08-03 09:35:56 -0700

    feat(data): adds support for data dirname (#188)
    * feat(data): adds support for data dirname
    
    * feat(tf): adds lifecycle to cloud functions, combines policy

--- cluster/functions/src/storageObjectDelete.ts
+++ cluster/functions/src/storageObjectDelete.ts
@@ -1,10 +1,12 @@
 import { Firestore } from '@google-cloud/firestore';
 import { Storage } from '@google-cloud/storage';
 import { CloudFunctionsContext } from '@google-cloud/functions-framework';
+import path from 'path';
+import crypto from 'crypto';
 
-const serviceKeyJson = process.env.GCLOUD_SERVICE_KEY
-if (serviceKeyJson == undefined) throw new Error('GCLOUD_SERVICE_KEY undefined')
-const serviceAccount = JSON.parse(serviceKeyJson)
+const serviceKeyJson = process.env.GCLOUD_SERVICE_KEY;
+if (serviceKeyJson == undefined) throw new Error('GCLOUD_SERVICE_KEY undefined');
+const serviceAccount = JSON.parse(serviceKeyJson);
 
 const firestore = new Firestore({
   projectId: serviceAccount.project_id,
@@ -22,6 +24,19 @@ export const storage = new Storage({
   },
 });
 
+function generateHash(input: string): { dirname: string; documentPath: string } {
+  const hash = crypto.createHash('sha256').update(input).digest('hex');
+
+  // dirname is the directory of the file relative to the project data folder
+  const segments = input.split(path.sep);
+  const basePath = path.join(segments[0], segments[1], segments[2]);
+  const dirname = path.relative(basePath, path.dirname(input));
+
+  const documentPath = path.join(basePath, hash);
+
+  return { dirname, documentPath };
+}
+
 export const storageObjectDelete = async (
   file: { name: string; bucket: string },
   context: CloudFunctionsContext
@@ -29,9 +44,10 @@ export const storageObjectDelete = async (
   // delete triggers when overwriting in gcsfuse
   // check to see if the file exists before removing the firestore doc
   const exists = (await storage.bucket(file.bucket).file(file.name).exists())[0];
+  const { documentPath } = generateHash(file.name);
   if (!exists) {
     console.log(`file ${file.name} doesn't exist, deleting`);
-    await firestore.doc(file.name).delete();
+    await firestore.doc(documentPath).delete();
   } else {
     console.log(`file ${file.name} exists, ignoring overwrite`);
   }
