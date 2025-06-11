40cb44af54f9c2432f673d2465c6572afb229a16
nst-t <41758588+nst-t@users.noreply.github.com>
2025-06-10 19:57:53 -0700

    updates container base to debian bookworm (#192)

--- cluster/integration-tests/browser-client/client/app/jest.puppeteer.config.js
+++ cluster/integration-tests/browser-client/client/app/jest.puppeteer.config.js
@@ -1,15 +1,18 @@
+const { exec } = require('child_process');
+
 module.exports = {
-    server: {
-        command: "npm run serve",
-        port: 3000,
-        launchTimeout: 10000,
-        debug: true,
-    },
-    launch: {
-        headless: !(process.env.NON_HEADLESS === 'true'),
-        slowMo: false,
-        devtools: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    },
-    browserContext: 'default'
+  server: {
+    command: 'npm run serve',
+    port: 3000,
+    launchTimeout: 10000,
+    debug: true,
+  },
+  launch: {
+    headless: !(process.env.NON_HEADLESS === 'true'),
+    slowMo: false,
+    devtools: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: '/usr/bin/chromium',
+  },
+  browserContext: 'default',
 };
