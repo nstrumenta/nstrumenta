04cb571badda17cc75f5dbed771106db21d459eb
nst-t <tyler@nstrumenta.com>
2025-08-29 02:34:05 +0000

    Remove unused Firestore import from edit-dialog component
    - Remove unused Firestore import and injection from constructor
    - Component doesn't actually use Firebase operations
    - This completes the Firebase centralization project

--- cluster/frontend/src/app/components/edit-dialog/edit-dialog.component.ts
+++ cluster/frontend/src/app/components/edit-dialog/edit-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Firestore } from '@angular/fire/firestore';
 
 @Component({
     selector: 'app-edit-dialog',
@@ -12,7 +11,6 @@ export class EditDialogComponent {
   newEmail: string;
 
   constructor(
-    private firestore: Firestore,
     public dialogRef: MatDialogRef<EditDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
