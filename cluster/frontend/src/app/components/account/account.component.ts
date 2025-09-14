62c9b5f91a9484703781a171effc6667cac49710
nst-t <tyler@nstrumenta.com>
2025-09-14 19:09:46 +0000

    Fix user settings display broken since v3.1.39
    - Restore router-outlet in AccountComponent to display UserProfileComponent
    - Improve user profile UI with actual avatar image instead of photoURL text
    - Add copy button for User ID with clipboard integration and snackbar feedback
    - Simplify navigation by removing links to non-existent routes
    - Use theme-safe styling to avoid white-on-white text issues
    - Make User ID prominent with monospace font and proper labeling
    
    Fixes issue where navigating to /account would not show user information including User ID.

--- cluster/frontend/src/app/components/account/account.component.ts
+++ cluster/frontend/src/app/components/account/account.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-account',
     templateUrl: './account.component.html',
-    styleUrls: ['./account.component.scss']
+    styleUrls: ['./account.component.scss'],
+    imports: [MatToolbar, RouterOutlet]
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
 }
--- cluster/frontend/src/app/components/user-profile/user-profile.component.ts
+++ cluster/frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,14 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
-import { MatList, MatListItem } from '@angular/material/list';
 import { AsyncPipe } from '@angular/common';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-user-profile',
     templateUrl: './user-profile.component.html',
     styleUrls: ['./user-profile.component.scss'],
-    imports: [MatList, MatListItem, AsyncPipe]
+    imports: [AsyncPipe, MatIconButton, MatIcon, CdkCopyToClipboard]
 })
 export class UserProfileComponent {
   public authService = inject(AuthService);
+  private snackBar = inject(MatSnackBar);
+
+  onCopied(copied: boolean) {
+    if (copied) {
+      this.snackBar.open('User ID copied to clipboard!', 'Close', {
+        duration: 2000,
+      });
+    }
+  }
 }
