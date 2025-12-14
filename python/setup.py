d7bb820b27aff9b85b8f7842b9ddb2f3cadb787b
nst-t <tyler@nstrumenta.com>
2025-12-14 19:55:54 +0000

    chore: release v4.0.7

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nstrumenta',
-    version='4.0.6',
+    version='4.0.7',
     packages=find_packages(),
     install_requires=[
         'requests',
