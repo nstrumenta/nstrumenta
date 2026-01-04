c00ea9bb4d3673de615d6d82e66e0ef4177b7806
Tyler Bryant <tyler@nstrumenta.com>
2026-01-04 18:07:39 +0000

    chore: release v4.2.2

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nstrumenta',
-    version='4.2.1',
+    version='4.2.2',
     packages=find_packages(),
     install_requires=[
         'requests',
