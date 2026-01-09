a422389178e4c4101f476bd23d437b03671a8b7b
Tyler Bryant <tyler@nstrumenta.com>
2026-01-09 17:33:07 +0000

    fix(ci): remove legacy terraform/firebaseConfig.json fetch dependency from frontend build

--- frontend/fetchFirebaseConfigJson.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-fetch(
-  `https://storage.googleapis.com/${process.env.FIREBASE_PROJECT_ID}-config/firebaseConfig.json`
-)
-  .then((res) => res.json())
-  .then((firebaseConfig) => {
-    console.log(firebaseConfig);
-    fs.writeFileSync('src/environments/firebaseConfig.json', JSON.stringify(firebaseConfig));
-  });
